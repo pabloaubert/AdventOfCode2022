19c431c20b63c4b6e9bbbb300b3035f7f0cbbeff
pabloaubert <pablo.aubert@me.com>
2022-12-10 09:18:00 +0100

    [ADD] day 10 solutions

--- solutions/solution_10.js
+++ solutions/solution_10.js
@@ -1,11 +1,44 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 
+
+const calculateSignalStrength = (data, points) => {
+  let result = 0;
+  points.forEach(point => {
+    result += point * data[point - 1];
+  });
+  return result;
+}
+
+
 const main = () => {
   const content = readFileSync(path.join(process.cwd(), './resources/input_10.txt'), { encoding: 'utf-8' });
+  const instructions = content.split('\n');
 
-  console.log("Part 1: ", );
-  console.log("Part 2: ", );
+  const histogram = [1];
+  
+  instructions.forEach(inst => {
+    const [command, value] = inst.split(' ');
+
+    histogram.push(histogram.at(-1));
+    if (command === "addx") {
+      histogram.push(histogram.at(-1) + Number(value));
+    }
+  });
+  
+  const render = [];
+  histogram.forEach((pos, index) => {
+    const indexInRow = index % 40;
+    const isSprite = indexInRow === pos || indexInRow === pos - 1 || indexInRow === pos + 1;
+    render.push(isSprite? '#' : '.');
+  });
+  
+  const str = render.join('');
+  const lines = str.match(/.{1,40}/g) ?? [];
+  
+  console.log("Part 1: ", calculateSignalStrength(histogram, [20, 60, 100, 140, 180, 220]));
+  console.log("Part 2: \n")
+  console.log(lines.join('\n'));
 };
 
 export default main;
